b8fca0b9cd38e419e35c69bba6de9f18aaafad00
Prateek-Shetty <prateekshetty2552@gmail.com>
2025-04-05 14:40:55 +0530

    Navbar fixed

--- digigrub/src/app/about/page.tsx
+++ digigrub/src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { MainNav } from "@/components/main-nav"
-import { MobileNav } from "@/components/mobile-nav"
+// import { MobileNav } from "@/components/mobile-nav"
 import { Button } from "@/components/ui/button"
 import { Footer } from "@/components/footer"
 import Link from "next/link"
@@ -11,7 +11,7 @@ export default function AboutPage() {
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center">
           <MainNav />
-          <MobileNav />
+          {/* <MobileNav /> */}
           <div className="flex flex-1 items-center justify-end space-x-4">
             <nav className="flex items-center space-x-2">
               <Link href="/cart">
